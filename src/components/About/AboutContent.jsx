b0c765aabe98798651ea31a4cbad28631f8397fa
Ash Caplan <57927650+accaplan@users.noreply.github.com>
2021-08-11 09:26:06 +0200

    Update AboutContent.jsx

--- src/components/About/AboutContent.jsx
+++ src/components/About/AboutContent.jsx
@@ -15,7 +15,10 @@ class About extends Component {
             Join our Element chat <a href="https://matrix.to/#/#leisurecoop:matrix.org" target="_blank">Leisure Talk</a> on our decentralised communication network.
             </p>
             <p>
-            We are a sister organization of like-minded regenerative placemakers.<br />
+            We are open-devleopment, want to contribute? <a href="https://www.figma.com/file/szwpEjhApwakv2G64hzEPn/Leisure-Cooperative-Development?node-id=0%3A1" target="_blank">FigJam</a> to check it out.
+            </p>
+            <p>
+            We are a sister organization to like-minded regenerative placemakers.<br />
             <a href="https://idlehours.co" target="_blank">The Idle Hours Company</a><br />
             <a href="https://infiniteplaces.org" target="_blank">Infinite Places</a><br />
             <a href="https://enoughstructures.com.com" target="_blank">Enough Structures</a><br />
